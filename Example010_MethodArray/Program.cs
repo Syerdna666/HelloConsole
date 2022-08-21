4adecb352d6f83c28dfeaa86402fbaa8818b9fe9
Graceless <syerdnaswag@gmail.com>
2022-08-21 23:17:02 +0300

    Example 010 MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 13, 22, 28, 24, 53, 61, 73, 28 };
+
+int n = array.Length;
+int find = 28;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
